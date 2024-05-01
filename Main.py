e43b9019cb6f185d8c8ef645bdaa80c9b1f913bb
Ethan H <ethanh@gmail.com>
2024-05-01 14:29:32 +1000

    Setting up Pygame for Poker App

--- Main.py
+++ Main.py
@@ -3,6 +3,26 @@ import pygame
 ## Poker
 ## Pygame
 ## 1 deck game
-## play against dealer
+## play against Ai through class
 
+pygame.init()
+screen = pygame.display.set_mode((1080, 720))
+clock = pygame.time.Clock()
+running = True
 
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("green")
+
+    # RENDER YOUR GAME HERE
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60) ## Limits FPS to 60
