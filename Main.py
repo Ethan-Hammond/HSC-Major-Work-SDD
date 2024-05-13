0890bdc81a8e2e71fb9121a6fbc198619d449a6f
Ethan-Hammond <EHammond@student.saintaug.nsw.edu.au>
2024-05-13 15:22:06 +1000

    trying to learn how to manouver through different web pages cant find solution

--- Main.py
+++ Main.py
@@ -1,12 +1,12 @@
-from flask import Flask, jsonify, request
-import random
+from flask import Flask, jsonify, request, Blueprint, render_template
 import sys
 import requests
 import json
 import random
-from flask import Flask, jsonify, request
+from flask import redirect, url_for
 
 
+main = Blueprint('main', __name__)
 
 def source_data():
     global random_game, json_data
@@ -31,17 +31,17 @@ class game_data:
         self.price = price
         self.rating = rating
     def get_name(self):
-        return self.name
+        return self.name    # returns the name of the game
     def get_data(self):
-        return self.data
+        return self.data   # returns the data of the game
     def get_developer(self):
-        return self.developer
+        return self.developer   # returns the developer of the game
     def get_genre(self):
-        return self.genre
+        return self.genre  # returns the genre of the game
     def get_price(self):
-        return self.price
+        return self.price  # returns the price of the game
     def get_rating(self):
-        return self.rating
+        return self.rating  # returns the rating of the game
 
 source_data()
 game = game_data(random_game, json_data[random_game]['name'], json_data[random_game]['developer'], json_data[random_game]['price'], json_data[random_game]['positive'], json_data[random_game]['score_rank'])
@@ -52,17 +52,31 @@ print(game.get_developer())
 
 ## a random game generator to help with gamer block
 
+
+
+
+
+
+## Diffferent routes start here
+
+
 app = Flask(__name__)
 
-# games = ["Minecraft", "Fortnite", "Apex Legends", "League of Legennds", "Call of Duty"]
+@app.route('/', methods=['GET'])
+def home_page():
+    return render_template('base.html')
 
-
-@app.route('/suggest', methods=['GET'])
-def suggest():
+@app.route('/generator_page', methods=['GET'])
+def game_suggest():
     random_game = random.choice(list(json_data.keys()))
     game = game_data(random_game, json_data[random_game]['name'], json_data[random_game]['developer'], json_data[random_game]['price'], json_data[random_game]['positive'], json_data[random_game]['score_rank'])
-    return jsonify({"game": game.get_name(), "developer": game.get_developer()})
+    return jsonify({"game": game.get_name(), "developer": game.get_developer()}), render_template('generator.html')
+
+@app.route('/save_game', methods=['POST'])
+def save_game():
+    pass
+
 
 if __name__ == '__main__':
     app.run(debug=True)
-                
+    
