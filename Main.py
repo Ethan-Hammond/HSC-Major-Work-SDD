10cc46d1575df1519b3fad02d8db0b7466175172
Ethan-Hammond <EHammond@student.saintaug.nsw.edu.au>
2024-05-21 09:31:56 +1000

    Using the steam powered api to return the description
    I have implemented a system that sends requests to the steam powered api to return the description for the required game.
    The challenges i faced is that i originally was planning to use the steamspypi for the description but within the game dictionary i couldn't find the required information. Furthermore i have plans to clean the HTML that occasionally returns but using Beautiful Soup i couldn't getting  it to work.

--- Main.py
+++ Main.py
@@ -4,7 +4,7 @@ import requests
 import json
 import random
 from flask import redirect, url_for
-
+from bs4 import BeautifulSoup
 
 
 main = Blueprint('main', __name__)
@@ -12,23 +12,36 @@ main = Blueprint('main', __name__)
 def source_data():
     global random_game, json_data
     x = requests.get('https://steamspy.com/api.php?request=all')
-
     print(type(x.text))
     json_data = json.loads(x.text)
     ## creating dictonaries for the data inside of one big dictionary using
     ##json data to easily manuver through the data
-
     random_game = random.choice(list(json_data.keys()))
-    print(json_data[random_game])
-
-   
 
 
+
+def fetch_game_description(appid):
+    url = f'https://store.steampowered.com/api/appdetails?appids={appid}'
+    response = requests.get(url)  # Send a GET request to the URL
+    if response.status_code == 200: # Check if the response is successful
+        data = response.json()      # Convert the response to a JSON object
+        if data[str(appid)]['success']:  # checking if the request attained the information
+            return render_template(data[str(appid)]['data'].get('detailed_description', 'no description available')) # Get the 'about_the_game' field from the data
+    return 'No description available.' # Return this message if the request was unsuccessful
+
+# def clean_html(raw_html):
+#     soup = BeautifulSoup(raw_html, 'html.parser')
+#     soup.get_text()
+#     description = ['data'].get('about_the_game', 'No description available.')
+#     return soup(description)
+    
+## class to store the data of the game
 class game_data:
-    def __init__(self, appid, name, developer):
+    def __init__(self, appid, name, developer, ):
         self.appid = appid
         self.name = name
         self.developer = developer
+        #self.description = description
     def get_name(self):
         return self.name    # returns the name of the game
     def get_data(self):
@@ -41,9 +54,12 @@ class game_data:
         return self.price  # returns the price of the game
     def get_rating(self):
         return self.rating  # returns the rating of the game
+    def get_description(self):
+        return self.description
 
 source_data()
-game = game_data(random_game, json_data[random_game]['name'], json_data[random_game]['developer'])
+
+game = game_data(random_game, json_data[random_game]['name'], json_data[random_game]['developer'],)
 # print(game.get_developer())
 
 
@@ -64,7 +80,6 @@ def search_game_image(game_name):
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         soup = BeautifulSoup(response.text, 'html.parser')
-        
         # Find the first image result
         image_results = soup.find_all('a', class_='iusc')
         if image_results:
@@ -93,12 +108,16 @@ def home_page():
 
 @app.route('/generator_page', methods=['GET'])
 def game_suggest():
+    global appid
     random_game = random.choice(list(json_data.keys()))
-    game = game_data(random_game, json_data[random_game]['name'], json_data[random_game]['developer'])
+    appid = random_game
+    description = fetch_game_description(appid)
+    game = game_data(appid, json_data[random_game]['name'], json_data[random_game]['developer'], description )
     game_name = game.get_name()
     game_image = search_game_image(game_name)
+    
     print(game_image)
-    return render_template('generator_page.html', game_name=game_name, game_image=game_image)
+    return render_template('generator_page.html', game_name=game_name, game_image=game_image, game_description=game.get_description())
 @app.route('/save_game', methods=['POST'])
 def save_game():
     pass
